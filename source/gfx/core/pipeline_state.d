6406202ebcd47358bebfd23ec9bc8bba30d91563
Remi THEBAULT <remi.thebault@gmail.com>
2016-07-12 08:25:18 +0200

    pipeline_state attribs

--- source/gfx/core/pipeline_state.d
+++ source/gfx/core/pipeline_state.d
@@ -3,6 +3,9 @@ module gfx.core.pipeline_state;
 import gfx.core.rc;
 import gfx.core.context;
 
+import std.traits;
+import std.meta;
+
 /// pipeline state objects (pso) embed a part of the pipeline state
 /// in order to allow fast rendering context switches.
 /// PSOs have a layout part and a data part.
@@ -15,20 +18,46 @@ import gfx.core.context;
 /// backends that natively support psos are Vulkan and DX12 (none implemented at this stage)
 /// for other backends, the pipeline switch is emulated in a straightforward way
 
-template PipelineState(VT) {
 
-    import std.traits;
-    import std.meta;
 
-    template VertexAttrib(string n, T) {
-        alias name = n;
-        alias Type = T;
-    }
+/// UDA struct to associate vertex struct member name to vertex input attrib name
+/// it is to be applied on the vertex structure fields directly
+/// if a vertex structure field has no AttribName attribute, the member name is
+/// assumed as attribute name in the vertex shader
+struct AttribName {
+    string attrib;
+}
 
+/// template that represent a vertex attrib
+template VertexAttrib(T, string m, string a) {
+    alias Type = T;
+    enum member = m;
+    enum attrib = a;
+}
+
+/// template that analyses a vertex struct and resolves as an
+/// AliasSeq of VertexAttrib (in declaration order)
+template VertexAttribs(VT) if(is(VT == struct)) {
     alias FNames = FieldNameTuple!VT;
     alias FTypes = Fields!VT;
     enum FCount = FNames.length;
     static assert(FNames.length == FTypes.length);
+
+    alias AttribNameUDAs = getSymbolsByUDA!(VT, AttribName);
+
+    template ResolveAttribName(string memberName, size_t n) {
+        static if (n == AttribNameUDAs.length) {
+            enum ResolveAttribName = memberName; // defaults to member name
+        }
+        else {
+            static if (AttribNameUDAs[n].stringof == memberName) {
+                enum ResolveAttribName = getUDAs!(AttribNameUDAs[n], AttribName)[0].attrib;
+            }
+            else {
+                enum ResolveAttribName = ResolveAttribName!(memberName, n+1);
+            }
+        }
+    }
 
     template ParseAttribs(size_t n) {
         static if(n == FCount) {
@@ -36,14 +65,35 @@ template PipelineState(VT) {
         }
         else {
             alias ParseAttribs = AliasSeq!(
-                VertexAttrib!(FNames[0], FTypes[0]),
+                VertexAttrib!(FTypes[n], FNames[n], ResolveAttribName!(FNames[n], 0)),
                 ParseAttribs!(n+1)
             );
         }
     }
 
-    alias Attribs = ParseAttribs!0;
+    alias VertexAttribs = ParseAttribs!0;
+}
 
+
+version(unittest) {
+    struct VertexAttribTest {
+        @AttribName("a_Pos")
+        float[4] position;
+
+        float[4] color;
+    }
+
+    alias attribs = VertexAttribs!VertexAttribTest;
+
+    static assert(attribs.length == 2);
+    static assert(attribs[0].member == "position");
+    static assert(attribs[0].attrib == "a_Pos");
+    static assert(attribs[1].member == "color");
+    static assert(attribs[1].attrib == "color");
+}
+
+
+template PipelineState(VT) {
 
     class PipelineState {
         alias VertexType = VT;
