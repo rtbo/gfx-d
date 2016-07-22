fd74b60602c354328dba9aaf6f9b04fc955b56a0
Remi THEBAULT <remi.thebault@gmail.com>
2016-07-22 02:53:02 +0200

    unrelated types cannot be unsafeCasted

--- source/gfx/backend/package.d
+++ source/gfx/backend/package.d
@@ -3,7 +3,8 @@ module gfx.backend;
 
 package U unsafeCast(U, T)(T obj)
         if ((is(T==class) || is(T==interface)) &&
-            (is(U==class) || is(U==interface)))
+            (is(U==class) || is(U==interface)) &&
+            is(U : T))
 {
     debug {
         auto uObj = cast(U)obj;
@@ -17,7 +18,8 @@ package U unsafeCast(U, T)(T obj)
 
 package const(U) unsafeCast(U, T)(const(T) obj)
         if ((is(T==class) || is(T==interface)) &&
-            (is(U==class) || is(U==interface)))
+            (is(U==class) || is(U==interface)) &&
+            is(U : T))
 {
     debug {
         auto uObj = cast(const(U))obj;
