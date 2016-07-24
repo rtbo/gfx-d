049c042cf50948a729831fcd102493cf1877b507
Remi THEBAULT <remi.thebault@gmail.com>
2016-07-24 11:01:39 +0200

    glfw support for multiple windows

--- source/gfx/window/glfw.d
+++ source/gfx/window/glfw.d
@@ -13,14 +13,34 @@ import derelict.opengl3.gl3;
 
 import std.meta : allSatisfy;
 
+
+shared static this() {
+    import std.exception : enforce;
+    DerelictGLFW3.load();
+    DerelictGL3.load();
+    glfwSetErrorCallback(&handleError);
+    enforce(glfwInit());
+}
+
+shared static ~this() {
+    glfwMakeContextCurrent(null);
+    glfwTerminate();
+}
+
+
 extern(C)
-void handleError(int code, const(char)*str) nothrow {
+private void handleError(int code, const(char)*str) nothrow {
     try {
         import std.experimental.logger;
         import std.string : fromStringz;
         errorf("GLFW error: %s: %s", code, fromStringz(str));
     }
     catch(Throwable th) {}
+}
+
+extern(C)
+private void handleClose(GLFWwindow* window) nothrow {
+    glfwSetWindowUserPointer(window, null);
 }
 
 
@@ -30,45 +50,25 @@ class RawWindow : RefCounted {
     private GLFWwindow *_window;
     private Rc!GlDevice _device;
 
-    private void preInit(SurfaceType colorSurf, ubyte samples) {
-        import std.exception : enforce;
-
-        assert(isRender(colorSurf));
-
-        DerelictGLFW3.load();
-        DerelictGL3.load();
-
-        enforce(glfwInit());
-
-        glfwSetErrorCallback(&handleError);
-
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-        glfwWindowHint(GLFW_RED_BITS,       colorSurf.redBits);
-        glfwWindowHint(GLFW_GREEN_BITS,     colorSurf.greenBits);
-        glfwWindowHint(GLFW_BLUE_BITS,      colorSurf.blueBits);
-        glfwWindowHint(GLFW_ALPHA_BITS,     colorSurf.alphaBits);
-        glfwWindowHint(GLFW_SRGB_CAPABLE,   colorSurf.hasChannel(ChannelType.Srgb));
-        glfwWindowHint(GLFW_SAMPLES,        samples);
-        glfwWindowHint(GLFW_DOUBLEBUFFER,   1);
-    }
 
     /// constructor without depth/stencil buffer
     this(string title, ushort width, ushort height, ubyte samples, SurfaceType colorSurf)
     {
-        preInit(colorSurf, samples);
-        postInit(title, width, height);
+        setupGlContext();
+        setupColor(colorSurf);
+        setupMisc(samples);
+        setupWindow(title, width, height);
     }
 
     /// constructor with a depth and/or stencil buffer
     this(string title, ushort width, ushort height, ubyte samples,
             SurfaceType colorSurf, SurfaceType dsSurf)
     {
-        preInit(colorSurf, samples);
-        initDs(dsSurf);
-        postInit(title, width, height);
+        setupGlContext();
+        setupColor(colorSurf);
+        setupDs(dsSurf);
+        setupMisc(samples);
+        setupWindow(title, width, height);
     }
 
     /// constructor with a depth and a stencil buffer
@@ -76,34 +76,62 @@ class RawWindow : RefCounted {
             SurfaceType colorSurf, SurfaceType dsSurf1, SurfaceType dsSurf2)
     {
         /// TODO: assert depth xor stencil
-        preInit(colorSurf, samples);
-        initDs(dsSurf1); initDs(dsSurf2);
-        postInit(title, width, height);
+        setupGlContext();
+        setupColor(colorSurf);
+        setupDs(dsSurf1); setupDs(dsSurf2);
+        setupMisc(samples);
+        setupWindow(title, width, height);
     }
 
-    private void initDs(SurfaceType dsSurf) {
+    private void setupGlContext() {
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    }
+
+    private void setupColor(SurfaceType colSurf) {
+        assert(isRender(colSurf));
+        glfwWindowHint(GLFW_RED_BITS,       colSurf.redBits);
+        glfwWindowHint(GLFW_GREEN_BITS,     colSurf.greenBits);
+        glfwWindowHint(GLFW_BLUE_BITS,      colSurf.blueBits);
+        glfwWindowHint(GLFW_ALPHA_BITS,     colSurf.alphaBits);
+        glfwWindowHint(GLFW_SRGB_CAPABLE,   colSurf.hasChannel(ChannelType.Srgb));
+
+        // forget previous setup
+        glfwWindowHint(GLFW_DEPTH_BITS, 0);
+        glfwWindowHint(GLFW_STENCIL_BITS, 0);
+    }
+
+    private void setupDs(SurfaceType dsSurf) {
         assert(isDepthStencil(dsSurf));
         if (dsSurf.isDepth) glfwWindowHint(GLFW_DEPTH_BITS, dsSurf.depthBits);
         if (dsSurf.isStencil) glfwWindowHint(GLFW_STENCIL_BITS, dsSurf.stencilBits);
     }
 
-    private void postInit(string title, ushort width, ushort height) {
+    private void setupMisc(ubyte samples) {
+        glfwWindowHint(GLFW_SAMPLES,        samples);
+        glfwWindowHint(GLFW_DOUBLEBUFFER,   1);
+    }
+
+    private void setupWindow(string title, ushort width, ushort height) {
         import std.exception : enforce;
         import std.string : toStringz;
 
         _window = glfwCreateWindow(width, height, toStringz(title), null, null);
         enforce(_window);
+        glfwSetWindowUserPointer(_window, cast(void*)this);
 
         makeCurrent();
-        DerelictGL3.reload();
+        shared static bool reloaded = false;
+        if (!reloaded) DerelictGL3.reload();
 
         _device = new GlDevice();
     }
 
+
+
     void drop() {
         _device.unload();
-        glfwMakeContextCurrent(null);
-        glfwTerminate();
     }
 
     @property inout(GLFWwindow)* glfwWindow() inout {
@@ -114,6 +142,10 @@ class RawWindow : RefCounted {
 
     @property bool shouldClose() const {
         return cast(bool)glfwWindowShouldClose(cast(GLFWwindow*)_window);
+    }
+
+    @property void shouldClose(bool val) {
+        glfwSetWindowShouldClose(_window, val);
     }
 
     void pollEvents() {
