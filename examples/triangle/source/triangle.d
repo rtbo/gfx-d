1fc0fe75169bcc5f65429a5f13af09f5c7d50030
Remi Thebault <remi.thebault@gmail.com>
2018-01-16 07:22:13 +0100

    refactor triangle

--- examples/triangle/source/triangle.d
+++ examples/triangle/source/triangle.d
@@ -5,6 +5,8 @@ import erupted;
 import gfx.core.rc;
 import gfx.graal;
 import gfx.graal.device;
+import gfx.graal.queue;
+import gfx.graal.sync;
 import gfx.vulkan;
 import gfx.window;
 
@@ -12,76 +14,156 @@ import std.algorithm;
 import std.exception;
 import std.stdio;
 
-int main() {
+class Triangle : Disposable
+{
+    Rc!Instance instance;
+    Window window;
+    uint graphicsQueueIndex;
+    uint presentQueueIndex;
+    Rc!PhysicalDevice physicalDevice;
+    Rc!Device device;
+    Queue graphicsQueue;
+    Queue presentQueue;
+    uint[2] surfaceSize;
+    Rc!Swapchain swapchain;
+    Image[] scImages;
+    Rc!Semaphore imageAvailableSem;
+    Rc!Semaphore renderingFinishSem;
 
-    vulkanInit();
+    void prepare() {
+        // initialize vulkan library
+        vulkanInit();
+        // create a vulkan instance
+        instance = createVulkanInstance("Triangle", VulkanVersion(0, 0, 1)).rc;
+        // create a window for the running platform
+        // the window surface is created during this process
+        window = createWindow(instance);
 
-    writeln("layers:");
-    vulkanInstanceLayers.each!(writeln);
-    writeln("extensions:");
-    vulkanInstanceExtensions.each!(writeln);
-
-    auto instance = createVulkanInstance("Triangle", VulkanVersion(0, 0, 1)).rc;
-
-    auto win = createWindow(instance);
-    scope(exit) win.close();
-
-    auto pd = enforce(
-        chooseDevice(instance, win.surface),
-        "Could not find a suitable device"
-    ).rc;
-
-    writeln("device layers:");
-    pd.vulkanDeviceLayers.each!(writeln);
-    writeln("device extensions:");
-    pd.vulkanDeviceExtensions.each!(writeln);
-    writefln("apiVersion = %s", VulkanVersion.fromUint(pd.apiVersion));
-    writefln("driverVersion = %s", VulkanVersion.fromUint(pd.driverVersion));
-    writefln("vendorId = %s", pd.vendorId);
-    writefln("deviceId = %s", pd.deviceId);
-    writefln("name = %s", pd.name);
-    writefln("type = %s", pd.type);
-    writefln("mem props = %s", pd.memoryProperties);
-    writefln("queue families = %s", pd.queueFamilies);
-    writefln("window surface support: %s", pd.supportsSurface(0, win.surface));
-    writefln("window surface caps: %s", pd.surfaceCaps(win.surface));
-    writefln("window surface formats: %s", pd.surfaceFormats(win.surface));
-    writefln("window surface presentModes: %s", pd.surfacePresentModes(win.surface));
-
-    const qfInd = chooseQueue(pd);
-    enforce (qfInd != invalidQueue, "Could not find a suitable graphics queue");
-
-    auto dev = pd.open([QueueRequest(qfInd, [0.5])]).rc;
-
-    const surfCaps = pd.surfaceCaps(win.surface);
-
-    enforce(surfCaps.usage & ImageUsage.transferDst, "TransferDst not supported by surface");
-    const usage = ImageUsage.transferDst | ImageUsage.colorAttachment;
-
-    const numImages = max(2, surfCaps.minImages);
-    enforce(surfCaps.maxImages == 0 || surfCaps.maxImages >= numImages);
-
-    uint[2] swapChainSize = [ 640, 480 ];
-    foreach (i; 0..2) {
-        swapChainSize[i] = clamp(swapChainSize[i], surfCaps.minSize[i], surfCaps.maxSize[i]);
+        // the rest of the preparation
+        prepareDevice();
+        prepareSwapchain();
+        prepareSync();
     }
 
-    const pm = choosePresentMode(pd, win.surface);
+    void prepareDevice()
+    {
+        bool checkDevice(PhysicalDevice dev) {
+            graphicsQueueIndex = uint.max;
+            presentQueueIndex = uint.max;
+            if (dev.softwareRendering) return false;
+            foreach (uint i, qf; dev.queueFamilies) {
+                const graphics = qf.cap & QueueCap.graphics;
+                const present = dev.supportsSurface(i, window.surface);
+                if (graphics && present) {
+                    graphicsQueueIndex = i;
+                    presentQueueIndex = i;
+                    return true;
+                }
+                if (graphics) graphicsQueueIndex = i;
+                if (present) presentQueueIndex = i;
+            }
+            return graphicsQueueIndex != uint.max && presentQueueIndex != uint.max;
+        }
+        foreach (pd; instance.devices) {
+            if (checkDevice(pd)) {
+                auto qrs = [ QueueRequest(graphicsQueueIndex, [ 0.5f ]) ];
+                if (graphicsQueueIndex != presentQueueIndex) {
+                    qrs ~= QueueRequest(presentQueueIndex, [ 0.5f ]);
+                }
+                physicalDevice = pd;
+                device = pd.open(qrs);
+                graphicsQueue = device.getQueue(graphicsQueueIndex, 0);
+                presentQueue = device.getQueue(presentQueueIndex, 0);
+                break;
+            }
+        }
+    }
 
-    const f = chooseFormat(pd, win.surface);
+    void prepareSwapchain() {
+        const surfCaps = physicalDevice.surfaceCaps(window.surface);
+        enforce(surfCaps.usage & ImageUsage.transferDst, "TransferDst not supported by surface");
+        const usage = ImageUsage.transferDst | ImageUsage.colorAttachment;
+        const numImages = max(2, surfCaps.minImages);
+        enforce(surfCaps.maxImages == 0 || surfCaps.maxImages >= numImages);
+        const f = chooseFormat(physicalDevice, window.surface);
+        surfaceSize = [ 640, 480 ];
+        foreach (i; 0..2) {
+            surfaceSize[i] = clamp(surfaceSize[i], surfCaps.minSize[i], surfCaps.maxSize[i]);
+        }
+        const pm = choosePresentMode(physicalDevice, window.surface);
 
-    auto sc = dev.createSwapchain(win.surface, pm, numImages, f, swapChainSize, usage).rc;
+        swapchain = device.createSwapchain(window.surface, pm, numImages, f, surfaceSize, usage);
+        scImages = swapchain.images;
+    }
 
-    auto scImgs = sc.images;
+    void prepareSync() {
+        imageAvailableSem = device.createSemaphore();
+        renderingFinishSem = device.createSemaphore();
+    }
+
+    void render() {
+        writeln("render!");
+    }
+
+    override void dispose() {
+        imageAvailableSem.unload();
+        renderingFinishSem.unload();
+        swapchain.unload();
+        device.unload();
+        physicalDevice.unload();
+        window.close();
+        instance.unload();
+    }
+}
+
+/// Return a format suitable for the surface.
+///  - if supported by the surface Format.rgba8_uNorm
+///  - otherwise the first format with uNorm numeric format
+///  - otherwise the first format
+Format chooseFormat(PhysicalDevice pd, Surface surface)
+{
+    const formats = pd.surfaceFormats(surface);
+    enforce(formats.length, "Could not get surface formats");
+    if (formats.length == 1 && formats[0] == Format.undefined) {
+        return Format.rgba8_uNorm;
+    }
+    foreach(f; formats) {
+        if (f == Format.rgba8_uNorm) {
+            return f;
+        }
+    }
+    foreach(f; formats) {
+        if (f.formatDesc.numFormat == NumFormat.uNorm) {
+            return f;
+        }
+    }
+    return formats[0];
+}
+
+PresentMode choosePresentMode(PhysicalDevice pd, Surface surface)
+{
+    auto modes = pd.surfacePresentModes(surface);
+    if (modes.canFind(PresentMode.mailbox)) {
+        return PresentMode.mailbox;
+    }
+    assert(modes.canFind(PresentMode.fifo));
+    return PresentMode.fifo;
+}
+
+int main() {
+
+    auto triangle = new Triangle();
+    triangle.prepare();
+    scope(exit) triangle.dispose();
 
     bool exitFlag;
-    win.mouseOn = (uint, uint) {
+    triangle.window.mouseOn = (uint, uint) {
         exitFlag = true;
     };
 
     while (!exitFlag) {
-        win.waitAndDispatch();
-        writeln("draw!");
+        triangle.window.waitAndDispatch();
+        triangle.render();
     }
 
     return 0;
@@ -110,44 +192,18 @@ enum invalidQueue = uint.max;
 
 /// Return the first queue index with graphics capability,
 /// or invalidQueue if none is found
-uint chooseQueue(PhysicalDevice pd) {
+uint chooseGraphicsQueue(PhysicalDevice pd) {
      foreach (i, qf; pd.queueFamilies) {
          if (qf.cap & QueueCap.graphics) return cast(uint)i;
      }
      return invalidQueue;
 }
 
-/// Return a format suitable for the surface.
-///  - if supported by the surface Format.rgba8_uNorm
-///  - otherwise the first format with uNorm numeric format
-///  - otherwise the first format
-Format chooseFormat(PhysicalDevice pd, Surface surface)
-{
-    auto formats = pd.surfaceFormats(surface);
-    enforce(formats.length, "Could not get surface formats");
-    if (formats.length == 1 && formats[0] == Format.undefined)
-    {
-        return Format.rgba8_uNorm;
-    }
-    foreach(f; formats) {
-        if (f == Format.rgba8_uNorm) {
-            return f;
-        }
-    }
-    foreach(f; formats) {
-        if (f.formatDesc.numFormat == NumFormat.uNorm) {
-            return f;
-        }
-    }
-    return formats[0];
-}
-
-PresentMode choosePresentMode(PhysicalDevice pd, Surface surface)
-{
-    auto modes = pd.surfacePresentModes(surface);
-    if (modes.canFind(PresentMode.mailbox)) {
-        return PresentMode.mailbox;
-    }
-    assert(modes.canFind(PresentMode.fifo));
-    return PresentMode.fifo;
+/// Return the first queue index with graphics capability,
+/// or invalidQueue if none is found
+uint choosePresentQueue(PhysicalDevice pd) {
+     foreach (i, qf; pd.queueFamilies) {
+         if (qf.cap & QueueCap.graphics) return cast(uint)i;
+     }
+     return invalidQueue;
 }
