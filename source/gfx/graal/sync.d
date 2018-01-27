73df4a48aa6d5aca720ce9f58b505c3237080901
Remi Thebault <remi.thebault@gmail.com>
2018-01-27 10:02:12 +0100

    add fence convenience

--- source/gfx/graal/sync.d
+++ source/gfx/graal/sync.d
@@ -1,6 +1,7 @@
 /// synchronization module
 module gfx.graal.sync;
 
+import core.time : dur, Duration;
 import gfx.core.rc;
 
 interface Semaphore : AtomicRefCounted
@@ -9,4 +10,6 @@ interface Semaphore : AtomicRefCounted
 interface Fence : AtomicRefCounted
 {
     @property bool signaled();
+    void reset();
+    void wait(Duration timeout=dur!"seconds"(-1));
 }
--- source/gfx/vulkan/sync.d
+++ source/gfx/vulkan/sync.d
@@ -2,6 +2,8 @@ module gfx.vulkan.sync;
 
 package:
 
+import core.time : dur, Duration;
+
 import erupted;
 
 import gfx.core.rc;
@@ -27,5 +29,26 @@ class VulkanFence : VulkanDevObj!(VkFence, vkDestroyFence), Fence
 
     override @property bool signaled() {
         return vkGetFenceStatus(vkDev, vk) == VK_SUCCESS;
+    }
+
+    override void reset()
+    {
+        auto vkF = vk;
+        vulkanEnforce(
+            vkResetFences(vkDev, 1, &vkF),
+            "Could not reset Vulkan fence"
+        );
+    }
+
+    override void wait(Duration timeout=dur!"seconds"(-1))
+    {
+        const nsecs = timeout.total!"nsecs";
+        const vkTimeout = nsecs < 0 ? ulong.max : cast(ulong)nsecs;
+        auto vkF = vk;
+
+        vulkanEnforce(
+            vkWaitForFences(vkDev, 1, &vkF, VK_TRUE, vkTimeout),
+            "Could not wait for Vulkan fence"
+        );
     }
 }
