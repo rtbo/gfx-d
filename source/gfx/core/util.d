91d069acb2d8af3e588e279389f20d9d1e2ddc7d
Remi Thebault <remi.thebault@gmail.com>
2018-02-06 23:50:22 +0100

    clarify unsafeCast intent

--- source/gfx/core/util.d
+++ source/gfx/core/util.d
@@ -5,43 +5,57 @@ module gfx.core.util;
 /// If T is a base class of U, this does a blind cast to the same address.
 /// If T is an interface implemented by U, it first offset address to the object base address
 /// and does a blind cast to that address.
-U unsafeCast(U, T)(T obj)
-        if ((is(T==class) || is(T==interface)) &&
-            is(U==class) && is(U : T))
+template unsafeCast(U) if (!is(U == const))
 {
-    if (!obj) return null;
-    debug {
-        auto uObj = cast(U)obj;
-        assert(uObj, "unsafeCast from "~T.stringof~" to "~U.stringof~" failed");
-        return uObj;
-    }
-    else {
-        static if (is(T == interface)) {
-            return cast(U)(cast(void*)(cast(Object)obj));
+    static assert (!is(U == interface), "unsafeCast cannot be used to return interfaces");
+    static assert (is(U == class), "unsafeCast can only be used with objects");
+
+    @system
+    U unsafeCast(T)(T obj)
+            if ((is(T==class) || is(T==interface)) && is(U : T))
+    {
+        if (!obj) return null;
+        debug {
+            auto uObj = cast(U)obj;
+            assert(uObj, "unsafeCast from "~T.stringof~" to "~U.stringof~" failed");
+            return uObj;
         }
         else {
-            return cast(U)(cast(void*)obj);
+            static if (is(T == interface)) {
+                return cast(U)(cast(void*)(cast(Object)obj));
+            }
+            else {
+                return cast(U)(cast(void*)obj);
+            }
+        }
+    }
+
+}
+
+/// ditto
+template unsafeCast(U) if (is(U == const))
+{
+    static assert (!is(U == interface), "unsafeCast cannot be used to return interfaces");
+    static assert (is(U == class), "unsafeCast can only be used with objects");
+
+    @system
+    U unsafeCast(T)(const(T) obj)
+            if ((is(T==class) || is(T==interface)) && is(U : const(T)))
+    {
+        if (!obj) return null;
+        debug {
+            auto uObj = cast(U)obj;
+            assert(uObj, "unsafeCast from "~T.stringof~" to "~U.stringof~" failed");
+            return uObj;
+        }
+        else {
+            static if (is(T == interface)) {
+                return cast(U)(cast(const(void*))(cast(const(Object))obj));
+            }
+            else {
+                return cast(U)(cast(const(void*))obj);
+            }
         }
     }
 }
 
-/// ditto
-const(U) unsafeCast(U, T)(const(T) obj)
-        if ((is(T==class) || is(T==interface)) &&
-            is(U==class) && is(U : T))
-{
-    if (!obj) return null;
-    debug {
-        auto uObj = cast(const(U))obj;
-        assert(uObj, "unsafeCast from "~T.stringof~" to "~U.stringof~" failed");
-        return uObj;
-    }
-    else {
-        static if (is(T == interface)) {
-            return cast(const(U))(cast(const(void*))(cast(const(Object))obj));
-        }
-        else {
-            return cast(const(U))(cast(const(void*))obj);
-        }
-    }
-}
