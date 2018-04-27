23e86eb187a492f89d66b4cd8d42af793a767890
Remi Thebault <remi.thebault@gmail.com>
2018-04-28 00:53:16 +0200

    fixing lookAt for rh coords

--- math/source/gfx/math/util.d
+++ math/source/gfx/math/util.d
@@ -9,15 +9,15 @@ auto lookAt(V)(in V eye, in V target, in V up) if (isVec!V)
 {
     alias T = V.Component;
 
-    const z = normalize(target - eye);
+    const z = normalize(eye - target);
     const x = normalize(cross(z, up));
     const y = cross(x, z);
 
     return Mat4!T(
-        x,      -dot(x, eye),
-        y,      -dot(y, eye),
-       -z,      -dot(z, eye),
-        0, 0, 0, 1,
+        Vec4!T( x,      -dot(x, eye)),
+        Vec4!T( y,      -dot(y, eye)),
+        Vec4!T( z,      -dot(z, eye)),
+        Vec4!T( 0, 0, 0,    1       ),
     );
 }
 
