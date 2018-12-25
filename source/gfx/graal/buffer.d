4c6327135365b7cf6ede77427c4668006953d3d1
Remi Thebault <remi.thebault@gmail.com>
2018-12-25 15:58:13 +0100

    rm some contract
    Makes link stage fail in some cases

--- source/gfx/graal/buffer.d
+++ source/gfx/graal/buffer.d
@@ -32,15 +32,9 @@ interface Buffer : IAtomicRefCounted
     @property size_t size();
     @property BufferUsage usage();
     @property MemoryRequirements memoryRequirements();
+
     /// The buffer keeps a reference to the device memory
-    void bindMemory(DeviceMemory mem, in size_t offset)
-    in {
-        assert(mem);
-        assert(!boundMemory);
-    }
-    out {
-        assert(boundMemory);
-    }
+    void bindMemory(DeviceMemory mem, in size_t offset);
 
     @property DeviceMemory boundMemory();
 
