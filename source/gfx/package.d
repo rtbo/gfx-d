3d6f0964fb4bc806605c379fe87850d2d6820833
Remi Thebault <remi.thebault@gmail.com>
2018-12-27 13:12:12 +0100

    add gfxLog

--- source/gfx/package.d
+++ source/gfx/package.d
@@ -1,6 +1,10 @@
 module gfx;
 
+import gfx.core.log : LogTag;
+
+enum gfxLogMask = 0xF000_0000;
+package immutable gfxLog = LogTag("GFX", gfxLogMask);
 
 enum uint gfxVersionMaj = 0;
-enum uint gfxVersionMin = 0;
-enum uint gfxVersionMic = 0;
+enum uint gfxVersionMin = 1;
+enum uint gfxVersionMic = 2;
